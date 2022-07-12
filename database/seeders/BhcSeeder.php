fceaa1e95ac015e81fcfc5c8afa768541d08a21e
David Mendoza <davidmendozaofficial@gmail.com>
2022-07-12 15:57:03 +0800

    fix bhc seeder
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- database/seeders/BhcSeeder.php
+++ database/seeders/BhcSeeder.php
@@ -22,7 +22,7 @@ class BhcSeeder extends Seeder
 
     public function addBhc($i){
         $bhc = new Bhc();
-        $bhc->rhu_id = rand(1, 3);
+        $bhc->rhu_id = rand(2, 4);
         $bhc->code = rand(10000, 99999);
         $bhc->name = "BHC$i";
         $bhc->region = "BHC$i - Region";
