baa5d9ff25e477a6c366eb6a692a0a238b37cf3f
David Mendoza <davidmendozaofficial@gmail.com>
2022-08-02 23:33:45 +0800

    Redirect Approver to request
    Signed-off-by: David Mendoza <davidmendozaofficial@gmail.com>

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\{Request as Req, Data, Alert};
+use Auth;
 
 class DashboardController extends Controller
 {
     function index(){
+        if(auth()->user()->role == "Approver"){
+            return redirect()->route('request.request')->send();
+        }
+
         // GET REQUESTS
         $requests = Req::whereBetween("transaction_date", [now()->startOfMonth(), now()->endOfMonth()])->select("transaction_date")->get();
         $rtm = $requests->count();
